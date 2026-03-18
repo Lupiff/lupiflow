4e569eb519ec3d7b4689c16f9d6fed9f078ecbe4
Consoli <matheusconsoli310@gmail.com>
2026-03-18 18:24:05 -0300

    passos iniciais do sistema gacha x1 elaborados

--- assets/resources/jogadoras/PlayerData.gd
+++ assets/resources/jogadoras/PlayerData.gd
@@ -3,7 +3,7 @@ class_name PlayerData
 
 @export var name: String = ""
 @export var number: int = 0       # número da camisa
-@export var position: String = "" # GOL, ZAG, MEI, ATA...
+@export var position: String = "" # GOL, ZAG, MC, ATA...
 @export var chute: int = 0
 @export var velocidade: int = 0
 @export var passe: int = 0
--- cenas/gacha_tela.gd
+++ cenas/gacha_tela.gd
@@ -2,8 +2,10 @@ extends Control
 
 
 # Called when the node enters the scene tree for the first time.
+#Carrega as cenas das cartas
 func _ready() -> void:
-	print(sortearUmaJogadora(jogadoras))
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,7 +31,41 @@ func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://cenas/tela inicial.tscn")
 
 
-func sortearUmaJogadora(array:Array) -> String:
+func sortearUmaJogadora(array:Array) -> PlayerData:
 	var sorteada = array.pick_random()
-	return sorteada.name
+	return sorteada
 	
+
+func descobrirRaridade(player:PlayerData) -> String:
+	if player.raridade == "Lendaria":
+		return "Lendaria"
+	elif player.raridade == "Rara":
+		return "Rara"
+	else:
+		return "Normal"
+
+
+
+func _on_wish_x_1_pressed() -> void:
+	const raraCena = preload("res://cenas/cartas/carta_rara.tscn")
+	const lendariaCena = preload("res://cenas/cartas/carta_lendaria.tscn")
+	const normalCena = preload("res://cenas/cartas/carta_normal.tscn")
+	
+	var jogadoraSorteada = sortearUmaJogadora(jogadoras)
+	var raridade:String = descobrirRaridade(jogadoraSorteada)
+	
+	if raridade == "Rara":
+		var spawnRara = raraCena.instantiate()
+		add_child(spawnRara)
+		spawnRara.setup(jogadoraSorteada)
+		
+	elif raridade == "Lendaria":
+		var spawnLendaria = lendariaCena.instantiate()
+		add_child(spawnLendaria)
+		spawnLendaria.setup(jogadoraSorteada)
+		
+	else:
+		var spawnNormal = normalCena.instantiate()
+		add_child(spawnNormal)
+		spawnNormal.setup(jogadoraSorteada)
+		
