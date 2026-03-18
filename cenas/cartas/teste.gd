205ad2606b93cf2586f58385ef46f2f4f59dcef4
Luis <luisfelipe281198@gmail.com>
2026-03-18 15:42:07 -0400

    testando como q as carta vai aparecer sla

--- cenas/cartas/teste.gd
+++ cenas/cartas/teste.gd
@@ -1,25 +1,27 @@
 extends Control
 
-@onready var carta_normal = $carta_normal
-@onready var carta_rara = $carta_rara
-@onready var carta_lendaria = $carta_lendaria
-@onready var carta_lendaria2 = $carta_lendaria2
-@onready var carta_lendaria3 = $carta_lendaria3
+
+const CartaNormal   = preload("res://cenas/cartas/carta_normal.tscn")
+const CartaRara     = preload("res://cenas/cartas/carta_rara.tscn")
+const CartaLendaria = preload("res://cenas/cartas/carta_lendaria.tscn")
+
+@onready var container = $GridContainer
+
+var jogadoras_teste = [
+	preload("res://assets/resources/jogadoras/GACHA/Cristiane_Rosa.tres"),
+	preload("res://assets/resources/jogadoras/Corinthians/Mia_de_Graaf.tres"),
+	preload("res://assets/resources/jogadoras/GACHA/Nayara.tres"),
+	preload("res://assets/resources/jogadoras/GACHA/Leona_Meissi.tres")
+]
 
 
 func _ready() -> void:
-	var jogador1 = preload("res://assets/resources/jogadoras/Flamengo/Varela.tres")
-	carta_normal.setup(jogador1)
-	
-	var jogador2 = preload ("res://assets/resources/jogadoras/GACHA/Cristiane_Rosa.tres")
-	carta_lendaria.setup(jogador2)
-	
-	var jogador3 = preload("res://assets/resources/jogadoras/GACHA/Mariana_Dias.tres")
-	carta_rara.setup(jogador3)
-	
-	
-	var jogador4 = preload("res://assets/resources/jogadoras/GACHA/Nayara.tres")
-	carta_lendaria2.setup(jogador4)
-	
-	var jogador5 = preload("res://assets/resources/jogadoras/GACHA/Leona_Meissi.tres")
-	carta_lendaria3.setup(jogador5)
+	for jogador in jogadoras_teste:
+		var carta
+		match jogador.raridade:
+			"Normal":   carta = CartaNormal.instantiate()
+			"Rara":     carta = CartaRara.instantiate()
+			"Lendaria": carta = CartaLendaria.instantiate()
+		
+		container.add_child(carta)
+		carta.setup(jogador)
