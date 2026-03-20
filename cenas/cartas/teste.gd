02759d51c5ab82405496b119581ce18afa5bb45a
Luis <luisfelipe281198@gmail.com>
2026-03-20 13:05:05 -0400

    fotos jogadoras flamengo

--- cenas/cartas/teste.gd
+++ cenas/cartas/teste.gd
@@ -1,27 +1,12 @@
 extends Control
 
-
-const CartaNormal   = preload("res://cenas/cartas/carta_normal.tscn")
-const CartaRara     = preload("res://cenas/cartas/carta_rara.tscn")
-const CartaLendaria = preload("res://cenas/cartas/carta_lendaria.tscn")
-
-@onready var container = $GridContainer
-
-var jogadoras_teste = [
-	preload("res://assets/resources/jogadoras/GACHA/Cristiane_Rosa.tres"),
-	preload("res://assets/resources/jogadoras/Corinthians/Mia_de_Graaf.tres"),
-	preload("res://assets/resources/jogadoras/GACHA/Nayara.tres"),
-	preload("res://assets/resources/jogadoras/GACHA/Leona_Meissi.tres")
-]
-
+const CartaJogador = preload("res://cenas/cartas/carta_rara.tscn")
 
 func _ready() -> void:
-	for jogador in jogadoras_teste:
-		var carta
-		match jogador.raridade:
-			"Normal":   carta = CartaNormal.instantiate()
-			"Rara":     carta = CartaRara.instantiate()
-			"Lendaria": carta = CartaLendaria.instantiate()
-		
-		container.add_child(carta)
-		carta.setup(jogador)
+	# carrega um jogador de teste
+	var jogador = preload("res://assets/resources/jogadoras/Flamengo/Lais_Pereira.tres")
+	
+	# instancia a carta
+	var carta = CartaJogador.instantiate()
+	add_child(carta)
+	carta.setup(jogador)
